8899f1ee124de357573b458e32e5a8c17c257f90
Tyler Graham <tylergraham134@gmail.com>
2015-12-01 00:36:51 -0700

    completes initial api setup

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,5 +1,29 @@
 class Api::V1::IdeasController < ApplicationController
-  def index
+  respond_to :json, :html
 
+  def index
+    respond_with :api, :v1, Idea.all
+  end
+
+  def show
+    respond_with :api, :v1, Idea.find_by(id: params[:id])
+  end
+
+  def create
+    respond_with :api, :v1, Idea.create(idea_params)
+  end
+
+  def update
+    respond_with :api, :v1, Idea.update(params[:id], idea_params)
+  end
+
+  def destroy
+    respond_with :api, :v1, Idea.destroy(params[:id])
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body, :quality)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'api/v1/ideas#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :ideas
+    end
+  end
 end
