bb66354ff10348c7c709e4a2b2002120d099b9f5
Tyler Graham <tylergraham134@gmail.com>
2015-12-01 18:27:50 -0700

    finishes add and delete methods in jquery

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,31 +1,49 @@
-$(document).ready(function () {
-  getIdeas()
-  bindSubmitButton();
-});
+var ideaBox = {
+  init: function(){
+    ideaBox.getIdeas();
+    ideaBox.bindSubmitButton();
+  },
 
-function getIdeas() {
-  $.ajax({
-    type: 'GET',
-    url: 'api/v1/ideas',
-    success: function(response) {
-      var ideaElements = $.map(response, function (idea, index) {
+  getIdeas: function(){
+    $.getJSON('api/v1/ideas', function(response){
+      var $ideaElements = $.map(response, function (idea, index) {
         var $listItem = $('<li></li>')
+        .addClass('idea')
+        .attr('id', idea.id)
         .append("<h3>" + idea.title + "</h3>")
-        .append("<p>" + idea.body + "</p>");
+        .append("<p>" + "Description: " + idea.body + "</p>")
+        .append("<p>" + "Quality: " + idea.quality + "</p>")
+        .append("<button class='delete-button'>" + "Delete" + "</button>");
         return $listItem;
       });
-      $(".idea-list").html(ideaElements);
-    }
-  });
-}
+      $(".idea-list").html($ideaElements);
+      ideaBox.deleteIdea();
+    });
+  },
 
-function bindSubmitButton() {
-  $('.new-idea').on('submit', function (event) {
-    event.preventDefault();
-    var idea = {idea: {
-      title: $('#new-idea-title').val(),
-      body: $('#new-idea-body').val(),
-    } };
-    $.post('/api/v1/ideas', idea).then(getIdeas);
-  });
-}
+  bindSubmitButton: function(){
+    $('.new-idea').on('submit', function (event) {
+      event.preventDefault();
+      var $data = $( this ).serialize();
+      $.post( '/api/v1/ideas', $data, function(){
+        ideaBox.getIdeas();}, 'JSON' );
+        $( this ).trigger( 'reset' );
+    });
+  },
+
+  deleteIdea: function(){
+    $('.delete-button').on('click', function(){
+      var $idea = $(this).closest('.idea');
+      $.ajax( {
+        url: '/api/v1/ideas/' + $idea.attr('id'),
+        type: 'DELETE',
+        dataType: 'JSON',
+        success: function(){
+          $idea.remove();
+        }
+      });
+    });
+  }
+};
+
+$(document).ready(ideaBox.init);
