c0bd267f84bd9fe05fa6875a699aaebd0a5a5edf
Tyler Graham <tylergraham134@gmail.com>
2015-12-03 02:45:03 -0700

    implements thumbs up functionality

--- public/assets/application-936cab0d1d3ddf88626562d3706ba3092dfa83765d2cefb7ccf91c3f7da6f55f.js
+++ public/assets/application-936cab0d1d3ddf88626562d3706ba3092dfa83765d2cefb7ccf91c3f7da6f55f.js
@@ -13192,13 +13192,17 @@ var ideaBox = {
         var $listItem = $('<li></li>')
         .addClass('idea')
         .attr('id', idea.id)
-        .append("<h3>" + idea.title + "</h3>")
-        .append("<p>" + "Description: " + idea.body + "</p>")
-        .append("<p>" + "Quality: " + idea.quality + "</p>")
+        .append("<h3 class='idea-title'>" + idea.title + "</h3>")
+        .append("<p class='idea-body'>" + idea.body + "</p>")
+        .append("<p class='idea-quality'>" + "Quality: " + "<span id='idea-quality'>" + "<i>" + idea.quality + "</i>" + "</span>"+ "</p>")
+        .append("<button id='thumbs-up-button'>" + "Thumbs Up!" + "</button>&nbsp")
+        .append("<button id='thumbs-down-button'>" + "Thumbs Down!" + "</button>&nbsp&nbsp&nbsp")
         .append("<button class='delete-button'>" + "Delete" + "</button>" + "<br><br>");
         return $listItem;
       });
       $(".idea-list").html($ideaElements.reverse());
+      ideaBox.editIdea();
+      ideaBox.thumbsUpDown();
       ideaBox.deleteIdea();
     });
   },
@@ -13224,6 +13228,83 @@ var ideaBox = {
           $idea.remove();
         }
       });
+    });
+  },
+
+  editIdea: function(){
+    ideaBox.editTitle();
+    ideaBox.editBody();
+  },
+
+  editTitle: function(){
+    $('.idea-list').on('click', '.idea-title', function(){
+      $(this).attr('contenteditable', 'true')
+      .focus()
+      .on('blur', ideaBox.updateTitle)
+    });
+  },
+
+  updateTitle: function(event){
+    var $idea = $(this).closest('.idea');
+    var $title = $idea.find('.idea-title').text();
+    $.ajax({
+      type: 'PATCH',
+      url: '/api/v1/ideas/' + $idea.attr( 'id' ),
+      data: {idea: {title: $title}},
+      success: function(){
+        ideaBox.getIdeas();
+      }
+    });
+  },
+
+  editBody: function(){
+    $('.idea-list').one('click', '.idea-body', function(){
+      $(this).attr('contenteditable', 'true')
+      .focus()
+      .one('blur', ideaBox.updateBody)
+    });
+  },
+
+  updateBody: function(event){
+    var $idea = $(this).closest('.idea');
+    var $body = $idea.find('.idea-body').text();
+    $.ajax({
+      type: 'PATCH',
+      url: '/api/v1/ideas/' + idea.attr('id'),
+      data: {idea: {body: $body}},
+      success: function(){
+        ideaBox.getIdeas();
+      }
+    });
+  },
+
+  thumbsUpDown: function(){
+    ideaBox.thumbsUp();
+    // ideaBox.thumbsDown();
+  },
+
+  thumbsUp: function(){
+    $('.idea-list').on('click', '#thumbs-up-button', function(){
+      var $idea = $(this).closest('.idea');
+      var $quality = $idea.find('#idea-quality').text();
+      if($quality === "swill") {
+          ideaBox.updateQuality($idea, "plausible");
+      } else if($quality === "plausible") {
+          ideaBox.updateQuality($idea, "genius");
+      } else {
+          console.log("Idea already rated genius!")
+      }
+    })
+  },
+
+  updateQuality: function($idea, quality){
+    $.ajax({
+      type: 'PATCH',
+      url: '/api/v1/ideas/' + $idea.attr('id'),
+      data: {idea: {quality: quality}},
+      // success: function(){
+      //   ideaBox.getIdeas();
+      // },
     });
   }
 };
